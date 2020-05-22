8abd5bd5d6f5a25cbe6c09b670472df19646ae33
AceIsHuman <menoman1995@yahoo.com>
2020-05-21 20:46:08 -0500

    bug fix on move_to_front

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -92,10 +92,9 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
+        value = node.value
         self.delete(node)
-        node.next = self.head
-        self.head.prev = node
-        self.head = node
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
